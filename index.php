c415e145e4ee46b9402742099a5f4b2a353401fe
rohanbhangui <bhanguirohan@gmail.com>
2013-12-23 01:03:06 -0800

    initial commit

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <title>Sortable</title>
+  
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <script type="text/javascript" src="scripts/jquery-1.10.2.js"></script>
+  <script type="text/javascript" src="scripts/jquery-ui-1.10.3.js"></script>
+  <script type="text/javascript" src="scripts/jquery-migrate-1.1.0.js"></script>
+
+  <script type="text/javascript" src="scripts/index.js"></script>
+  <link rel="stylesheet" media="screen" type="text/css" href="css/index.css"/>
+
+  <link rel="shortcut icon" href="favicon.ico" />
+
+</head>
+<body>
+	<div class="table">
+		<div class="column">
+			<div class="cell"></div>
+			<div class="cell"></div>
+			<div class="cell"></div>
+			<div class="cell"></div>
+			<div class="cell"></div>
+			<div class="cell"></div>
+		</div>
+		<div class="column">
+			<div class="cell"></div>
+			<div class="cell"></div>
+			<div class="cell"></div>
+			<div class="cell"></div>
+			<div class="cell"></div>
+			<div class="cell"></div>
+		</div>
+		<div class="column">
+			<div class="cell"></div>
+			<div class="cell"></div>
+			<div class="cell"></div>
+			<div class="cell"></div>
+			<div class="cell"></div>
+			<div class="cell"></div>
+		</div>
+		<div class="column">
+			<div class="cell"></div>
+			<div class="cell"></div>
+			<div class="cell"></div>
+			<div class="cell"></div>
+			<div class="cell"></div>
+			<div class="cell"></div>
+		</div>
+	</div>
+</body>
+</html>
