e1111611af309b45e40e395df052a1d51976d877
rohanbhangui <bhanguirohan@gmail.com>
2013-12-23 20:46:31 -0800

    dblclicking header sorts rows

--- scripts/index.js
+++ scripts/index.js
@@ -1,64 +1,84 @@
 var users = [
-	{id: 1, name:"rohan", last: "bhangui", age: 18},
-	{id: 2, name:"karan", last: "bhangui", age: 23},
-	{id: 3, name:"greg", last: "methvin", age: 23},
-	{id: 4, name:"harvey", last: "multani", age: 26},
-	{id: 5, name:"safa", last: "tarik", age: 23},
-	{id: 6, name:"jack", last: "attack", age: 19},
-	{id: 7, name:"ikea", last: "summer", age: 45}
+  {id: 1, name:"rohan", last: "bhangui", age: 18},
+  {id: 2, name:"karan", last: "bhangui", age: 23},
+  {id: 3, name:"greg", last: "methvin", age: 23},
+  {id: 4, name:"harvey", last: "multani", age: 26},
+  {id: 5, name:"safa", last: "tarik", age: 23},
+  {id: 6, name:"jack", last: "attack", age: 19},
+  {id: 7, name:"ikea", last: "summer", age: 45}
 ];
 
 var categories = [];
 
 for(category in users[0]) {
-	categories.push(category);
+  categories.push(category);
 }
 
 function tableCreate() {
 
-	$(".table").html("");
+  $(".table").html("");
 
-	for(var i = 0; i < categories.length; i++) {
-		$(".table").append("<div class=\"column\"></div>");
-	}
+  for(var i = 0; i < categories.length; i++) {
+    $(".table").append("<div class=\"column\"></div>");
+  }
 
-	for(var i = 0; i <= users.length; i++) {
-		if(i == 0) {
-			$(".column").append("<div class=\"cell category\"></div>");
-		}
-		else {
-			$(".column").append("<div class=\"cell\"></div>");
-		}
-	}
+  for(var i = 0; i <= users.length; i++) {
+    if(i == 0) {
+      $(".column").append("<div class=\"cell category\"></div>");
+    }
+    else {
+      $(".column").append("<div class=\"cell\"></div>");
+    }
+  }
 
-	$(".column").each(function(index) {
-		$(this).children(".cell:nth-child(1)").html(categories[index]);
-	});
+  $(".column").each(function(index) {
+    $(this).children(".cell:nth-child(1)").html(categories[index]);
+  });
 
 }
 
 function tableUpdate() {
-	for(var i = 0; i < users.length; i++) {
-		for(var j = 0; j < categories.length; j++) {
-			$(".column:nth-child(" + (j+1) + ") .cell:nth-child(" + (i+2) + ")").html(users[i][categories[j]]);
-		}
-	}
+  for(var i = 0; i < users.length; i++) {
+    for(var j = 0; j < categories.length; j++) {
+      $(".column:nth-child(" + (j+1) + ") .cell:nth-child(" + (i+2) + ")").html(users[i][categories[j]]);
+    }
+  }
+
+  console.log("table has been updated");
+}
+
+function sortByKey(array, key) {
+    return array.sort(function(a, b) {
+        var x = a[key]; var y = b[key];
+        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+    });
 }
 
 $(document).ready(function () {
-	tableCreate();
-	tableUpdate();
+  tableCreate();
+  tableUpdate();
 
-	$(".table").sortable({
-		axis: "x",
-		cursor: "move",
-		cancel: ".row:not(:first-child) .cell",
-		activate: function(event, ui) {
-			console.log(event);
-			console.log($(this));
-			//event.currentTarget.toElement = $(".row .cell:nth-child(" + ($(event.currentTarget.toElement).index() + 1) + ")");
-		}
-	});
+  $(".table").sortable({
+    axis: "x",
+    cursor: "move",
+    cancel: ".column .cell:not(:first-child)",
+    activate: function(event, ui) {
+      console.log(event);
+      console.log($(this));
+      //event.currentTarget.toElement = $(".row .cell:nth-child(" + ($(event.currentTarget.toElement).index() + 1) + ")");
+    }
+  });
 
-	$(".table").disableSelection();
+  $(".table").disableSelection();
+
+  console.log(users);
+
+  $(".cell.category").on("dblclick", function() {
+    users = sortByKey(users, $(this).text());
+
+    console.log(users);
+
+    tableUpdate();
+  });
+  
 });
